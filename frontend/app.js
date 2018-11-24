8cdf84d33ed5138bbc965d86dfb7cdd9f36d9640
Axel Wikström <axel.wikstrom@gmail.com>
2018-11-24 17:56:51 +0200

    add some other sites

--- frontend/app.js
+++ frontend/app.js
@@ -1,26 +1,32 @@
-let sites = [[ 53.903192, -1.629799 ],
-[ 53.902806, -1.629651 ],
-[ 53.818928, -1.603168 ],
-[ 53.818772, -1.603207 ],
-[ 53.798325, -1.525394 ],
-[ 53.799408, -1.535259 ],
-[ 53.799186, -1.534937 ],
-[ 53.796056, -1.547675 ],
-[ 53.796259, -1.547696 ],
-[ 53.796012, -1.540937 ],
-[ 53.782096, -1.559061 ],
-[ 53.782096, -1.559061 ]];
+let sites = [
+    { lat: 53.903192, lon: -1.629799, id: 1 },
+    { lat: 53.799186, lon: -1.534937, id: 4 },
+    { lat: 53.796259, lon: -1.547696, id: 5 },
+    { lat: 53.782096, lon: -1.559061, id: 7 },
+]
 
-let global_data = [];
+let site_data = {};
 
-fetch('site_7.json').then(res => res.json().then(data => {
-    console.log(data[0]);
-    global_data = data;
-    initChart(global_data, 'Co2');
-}))
+currentSite = 7;
+let currentField = 'Co2'
+
+function loadData(siteId) {
+    function load() {
+        initChart(site_data[siteId], currentField);
+    }
+    if (site_data[siteId]) {
+        load();
+        return;
+    }
+    fetch(`site_${siteId}.json`).then(res => res.json().then(data => {
+        site_data[siteId] = data;
+        load();
+    }))
+}
 
 
-var mymap = L.map('map').setView([ 53.782096, -1.559061 ], 13);
+var mymap = L.map('map').setView([53.782096, -1.559061], 13);
+loadData(currentSite);
 
 addMarkers(sites);
 
@@ -32,9 +38,13 @@ var OpenStreetMap_Mapnik = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{
 
 OpenStreetMap_Mapnik.addTo(mymap);
 
-function addMarkers(points) {
-    points.forEach(point => {
-        let marker = L.marker(point);
+function addMarkers(site) {
+    site.forEach(site => {
+        let marker = L.marker([site.lat, site.lon]);
+        marker.on('click', () => {
+            currentSite = site.id;
+            loadData(currentSite);
+        })
         marker.addTo(mymap);
     })
 
@@ -84,5 +94,6 @@ function initChart(data, field) {
 let dropdown = document.getElementById("dropdown");
 
 dropdown.addEventListener('change', e => {
-    initChart(global_data, e.target.value);
+    currentField = e.target.value;
+    initChart(site_data[currentSite], currentField);
 })
