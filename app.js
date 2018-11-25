91339abb1fb65ddbb321d86ad8d20afbebac61a9
Uddalak Bhaduri <bhaduriu@gmail.com>
2018-11-25 04:47:07 +0200

    Update app.js

--- app.js
+++ app.js
@@ -42,8 +42,8 @@ function addMarkers(site) {
     site.forEach(site => {
         let marker = L.marker([site.lat, site.lon]);
         marker.on('click', () => {
-            var win = window.open('https://eu-west-1a.online.tableau.com/javascripts/api/viz_v1.js', '_blank');
-            win.focus();
+            var newWindow = window.open();
+            newWindow.document.write("<html><body><script type='text/javascript' src='https://eu-west-1a.online.tableau.com/javascripts/api/viz_v1.js'></script><div class='tableauPlaceholder' style='width: 1920px; height: 849px;'><object class='tableauViz' width='1920' height='849' style='display:none;'><param name='host_url' value='https%3A%2F%2Feu-west-1a.online.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='site_root' value='&#47;t&#47;hackjunction' /><param name='name' value='AvGCO2Plot&#47;Sheet2' /><param name='tabs' value='yes' /><param name='toolbar' value='yes' /><param name='showAppBanner' value='false' /><param name='filter' value='iframeSizedToWindow=true' /></object></div></body></html>");
         })
         marker.addTo(mymap);
     })
